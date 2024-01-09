Rocky5/xbmc4xbox-redux

commit 43bf25fad718d20073f69db1f40a99f2ab3a4dee

[65d8837] [all] another winapi nuke, QueryPerformanceCounter/QueryPerformanceFrequency replaced with CurrentHostCounter/CurrentHostFrequency, uses int64_t instead of LARGE_INTEGER. Need Linux/Win build testing.

xbmc/cores/dvdplayer/DVDClock.cpp
@@ -21,10 +21,11 @@
 #include "system.h"
 #include "DVDClock.h"
 #include "utils/SingleLock.h"
+#include "utils/TimeUtils.h"
 
 #include <math.h>
 
-LARGE_INTEGER CDVDClock::m_systemFrequency;
+int64_t CDVDClock::m_systemFrequency;
 CCriticalSection CDVDClock::m_systemsection;
 
 CDVDClock::CDVDClock()
@@ -33,10 +34,10 @@ CDVDClock::CDVDClock()
   CheckSystemClock();
 
   m_systemUsed = m_systemFrequency;
-  m_pauseClock.QuadPart = 0;
+  m_pauseClock = 0;
   m_bReset = true;
   m_iDisc = 0;
-  m_startClock.QuadPart = 0;
+  m_startClock = 0;
 }
 
 CDVDClock::~CDVDClock()
@@ -47,8 +48,8 @@ double CDVDClock::GetAbsoluteClock()
 {
   CheckSystemClock();
 
-  LARGE_INTEGER current;
-  QueryPerformanceCounter(&current);
+  int64_t current;
+  current = CurrentHostCounter();
 
   return SystemToAbsolute(current);
 }
@@ -56,15 +57,15 @@ double CDVDClock::GetAbsoluteClock()
 double CDVDClock::GetClock()
 {
   CSharedLock lock(m_critSection);
-  LARGE_INTEGER current;
-  QueryPerformanceCounter(&current);
+  int64_t current;
+  current = CurrentHostCounter();
   return SystemToPlaying(current);
 }
 
 double CDVDClock::GetClock(double& absolute)
 {
-  LARGE_INTEGER current;
-  QueryPerformanceCounter(&current);
+  int64_t current;
+  current = CurrentHostCounter();
   {
     CSingleLock lock(m_systemsection);
     CheckSystemClock();
@@ -82,31 +83,31 @@ void CDVDClock::SetSpeed(int iSpeed)
 
   if(iSpeed == DVD_PLAYSPEED_PAUSE)
   {
-    if(!m_pauseClock.QuadPart)
-      QueryPerformanceCounter(&m_pauseClock);
+    if(!m_pauseClock)
+      m_pauseClock = CurrentHostFrequency();
     return;
   }
   
-  LARGE_INTEGER current;
-  int64_t newfreq = m_systemFrequency.QuadPart * DVD_PLAYSPEED_NORMAL / iSpeed;
+  int64_t current;
+  int64_t newfreq = m_systemFrequency * DVD_PLAYSPEED_NORMAL / iSpeed;
   
-  QueryPerformanceCounter(&current);
-  if( m_pauseClock.QuadPart )
+  current = CurrentHostCounter();
+  if( m_pauseClock )
   {
-    m_startClock.QuadPart += current.QuadPart - m_pauseClock.QuadPart;
-    m_pauseClock.QuadPart = 0;
+    m_startClock += current - m_pauseClock;
+    m_pauseClock = 0;
   }
 
-  m_startClock.QuadPart = current.QuadPart - (int64_t)((double)(current.QuadPart - m_startClock.QuadPart) * newfreq / m_systemUsed.QuadPart);
-  m_systemUsed.QuadPart = newfreq;
+  m_startClock = current - (int64_t)((double)(current - m_startClock) * newfreq / m_systemUsed);
+  m_systemUsed = newfreq;
 }
 
 void CDVDClock::Discontinuity(double currentPts)
 {
   CExclusiveLock lock(m_critSection);
-  QueryPerformanceCounter(&m_startClock);
-  if(m_pauseClock.QuadPart)
-    m_pauseClock.QuadPart = m_startClock.QuadPart;
+  m_startClock = CurrentHostFrequency();
+  if(m_pauseClock)
+    m_pauseClock = m_startClock;
   m_iDisc = currentPts;
   m_bReset = false;
 }
@@ -114,52 +115,52 @@ void CDVDClock::Discontinuity(double currentPts)
 void CDVDClock::Pause()
 {
   CExclusiveLock lock(m_critSection);
-  if(!m_pauseClock.QuadPart)
-    QueryPerformanceCounter(&m_pauseClock);
+  if(!m_pauseClock)
+    m_pauseClock = CurrentHostFrequency();
 }
 
 void CDVDClock::Resume()
 {
   CExclusiveLock lock(m_critSection);
-  if( m_pauseClock.QuadPart )
+  if( m_pauseClock )
   {
-    LARGE_INTEGER current;
-    QueryPerformanceCounter(&current);
+    int64_t current;
+    current = CurrentHostCounter();
 
-    m_startClock.QuadPart += current.QuadPart - m_pauseClock.QuadPart;
-    m_pauseClock.QuadPart = 0;
+    m_startClock += current - m_pauseClock;
+    m_pauseClock = 0;
   }  
 }
 
 void CDVDClock::CheckSystemClock()
 {
-  if(!m_systemFrequency.QuadPart)
-    QueryPerformanceFrequency(&m_systemFrequency);
+  if(!m_systemFrequency)
+    m_systemFrequency = CurrentHostFrequency();
 }
 
-double CDVDClock::SystemToAbsolute(LARGE_INTEGER system)
+double CDVDClock::SystemToAbsolute(int64_t system)
 {
-  return DVD_TIME_BASE * (double)system.QuadPart / m_systemFrequency.QuadPart;
+  return DVD_TIME_BASE * (double)system / m_systemFrequency;
 }
 
-double CDVDClock::SystemToPlaying(LARGE_INTEGER system)
+double CDVDClock::SystemToPlaying(int64_t system)
 {
-  LARGE_INTEGER current;
+  int64_t current;
 
   if (m_bReset)
   {
     m_startClock = system;
     m_systemUsed = m_systemFrequency;
-    m_pauseClock.QuadPart = 0;
+    m_pauseClock = 0;
     m_iDisc = 0;
     m_bReset = false;
   }
   
-  if (m_pauseClock.QuadPart)
+  if (m_pauseClock)
     current = m_pauseClock;
   else
     current = system;
 
-  return DVD_TIME_BASE * (double)(current.QuadPart - m_startClock.QuadPart) / m_systemUsed.QuadPart + m_iDisc;
+  return DVD_TIME_BASE * (double)(current - m_startClock) / m_systemUsed + m_iDisc;
 }
 

xbmc/Application.cpp
@@ -1957,18 +1957,18 @@ void CApplication::LoadSkin(const SkinPtr& skin)
 
   g_localizeStrings.LoadSkinStrings(langPath, g_guiSettings.GetString("locale.language"));
 
-  LARGE_INTEGER start;
-  QueryPerformanceCounter(&start);
+  int64_t start;
+  start = CurrentHostCounter();
 
   CLog::Log(LOGINFO, "  load new skin...");
 
   // Load the user windows
   LoadUserWindows();
 
-  LARGE_INTEGER end, freq;
-  QueryPerformanceCounter(&end);
-  QueryPerformanceFrequency(&freq);
-  CLog::Log(LOGDEBUG,"Load Skin XML: %.2fms", 1000.f * (end.QuadPart - start.QuadPart) / freq.QuadPart);
+  int64_t end, freq;
+  end = CurrentHostCounter();
+  freq = CurrentHostFrequency();
+  CLog::Log(LOGDEBUG,"Load Skin XML: %.2fms", 1000.f * (end - start) / freq);
 
   CLog::Log(LOGINFO, "  initialize new skin...");
   m_guiPointer.AllocResources(true);

xbmc/Credits.cpp
@@ -1144,10 +1144,10 @@ void RunCredits()
   CloseHandle(s_hMusicStarted);
 
 #ifdef _DEBUG
-  LARGE_INTEGER freq, start, end, start2, end2;
-  __int64 rendertime = 0;
-  QueryPerformanceFrequency(&freq);
-  QueryPerformanceCounter(&start2);
+  int64_t freq, start, end, start2, end2;
+  int64_t rendertime = 0;
+  freq = CurrentHostFrequency();
+  start2 = CurrentHostCounter();
 #endif // _DEBUG
 
   // Start credits loop
@@ -1171,7 +1171,7 @@ void RunCredits()
     while (NextCredit < NUM_CREDITS || !ActiveList.empty())
     {
 #ifdef _DEBUG
-      QueryPerformanceCounter(&start);
+      start = CurrentHostCounter();
 #endif // _DEBUG
 
       if (WaitForSingleObject(hMusicThread, 0) == WAIT_TIMEOUT)
@@ -1358,21 +1358,21 @@ void RunCredits()
 
 #ifdef _DEBUG
       static char FPS[80];
-      QueryPerformanceCounter(&end);
-      rendertime += end.QuadPart - start.QuadPart;
+      end = CurrentHostCounter();
+      rendertime += end - start;
       if (++n == 50)
       {
-        QueryPerformanceCounter(&end2);
+        end2 = CurrentHostCounter();
 
         MEMORYSTATUS stat;
         GlobalMemoryStatus(&stat);
-        float f = float(end2.QuadPart - start2.QuadPart);
-        sprintf(FPS, "Render: %.2f fps (%.2f%%)\nFreeMem: %.1f/%uMB", 50.0f * freq.QuadPart / f, 100.f * rendertime / f,
+        float f = float(end2 - start2);
+        sprintf(FPS, "Render: %.2f fps (%.2f%%)\nFreeMem: %.1f/%uMB", 50.0f * freq / f, 100.f * rendertime / f,
                  float(stat.dwAvailPhys) / (1024.0f*1024.0f), stat.dwTotalPhys / (1024*1024));
 
         rendertime = 0;
         n = 0;
-        QueryPerformanceCounter(&start2);
+        start2 = CurrentHostCounter();
       }      
       CGUITextLayout::DrawText(g_fontManager.GetFont("font13"), 50, 30, 0xffffffff, 0, FPS, 0);
 #endif

xbmc/Util.cpp
@@ -101,6 +101,7 @@
 #include "utils/Crc32.h"
 #include "utils/RssReader.h"
 #include "settings/AdvancedSettings.h"
+#include "utils/TimeUtils.h"
 #include "utils/URIUtils.h"
 #include "cores/dvdplayer/DVDSubtitles/DVDSubtitleTagSami.h"
 #include "cores/dvdplayer/DVDSubtitles/DVDSubtitleStream.h"
@@ -3810,9 +3811,9 @@ void CUtil::BootToDash()
 void CUtil::InitRandomSeed()
 {
   // Init random seed 
-  LARGE_INTEGER now; 
-  QueryPerformanceCounter(&now); 
-  unsigned int seed = (now.LowPart);
+  int64_t now; 
+  now = CurrentHostCounter(); 
+  unsigned int seed = now;
 //  CLog::Log(LOGDEBUG, "%s - Initializing random seed with %u", __FUNCTION__, seed);
   srand(seed);
 }

xbmc/cores/dvdplayer/DVDClock.h
@@ -51,19 +51,19 @@ public:
   void SetSpeed(int iSpeed);
 
   static double GetAbsoluteClock();
-  static double GetFrequency() { return (double)m_systemFrequency.QuadPart ; }
+  static double GetFrequency() { return (double)m_systemFrequency ; }
 protected:
   static void CheckSystemClock();
-  static double SystemToAbsolute(LARGE_INTEGER system);
-  double SystemToPlaying(LARGE_INTEGER system);
+  static double SystemToAbsolute(int64_t system);
+  double SystemToPlaying(int64_t system);
 
   CSharedSection m_critSection;
-  LARGE_INTEGER m_systemUsed;  
-  LARGE_INTEGER m_startClock;
-  LARGE_INTEGER m_pauseClock;
+  int64_t m_systemUsed;
+  int64_t m_startClock;
+  int64_t m_pauseClock;
   double m_iDisc;
   bool m_bReset;
   
-  static LARGE_INTEGER m_systemFrequency;
+  static int64_t m_systemFrequency;
   static CCriticalSection m_systemsection;
 };

xbmc/guilib/GUIWindow.cpp
@@ -597,8 +597,8 @@ void CGUIWindow::AllocResources(bool forceLoad /*= FALSE */)
   CSingleLock lock(g_graphicsContext);
 
 #ifdef _DEBUG
-  LARGE_INTEGER start;
-  QueryPerformanceCounter(&start);
+  int64_t start;
+  start = CurrentHostCounter();
 #endif
   // use forceLoad to determine if xml file needs loading
   forceLoad |= (m_loadType == LOAD_EVERY_TIME);
@@ -624,30 +624,30 @@ void CGUIWindow::AllocResources(bool forceLoad /*= FALSE */)
     }
   }
 
-  LARGE_INTEGER slend;
-  QueryPerformanceCounter(&slend);
+  int64_t slend;
+  slend = CurrentHostCounter();
 
   // and now allocate resources
   g_TextureManager.StartPreLoad();
   CGUIControlGroup::PreAllocResources();
   g_TextureManager.EndPreLoad();
 
-  LARGE_INTEGER plend;
-  QueryPerformanceCounter(&plend);
+  int64_t plend;
+  plend = CurrentHostCounter();
 
   CGUIControlGroup::AllocResources();
 
   g_TextureManager.FlushPreLoad();
 #ifdef _DEBUG
-  LARGE_INTEGER end, freq;
-  QueryPerformanceCounter(&end);
-  QueryPerformanceFrequency(&freq);
+  int64_t end, freq;
+  end = CurrentHostCounter();
+  freq = CurrentHostFrequency();
   if (forceLoad)
-    CLog::Log(LOGDEBUG,"Alloc resources: %.2fms (%.2f ms skin load, %.2f ms preload)", 1000.f * (end.QuadPart - start.QuadPart) / freq.QuadPart, 1000.f * (slend.QuadPart - start.QuadPart) / freq.QuadPart, 1000.f * (plend.QuadPart - slend.QuadPart) / freq.QuadPart);
+    CLog::Log(LOGDEBUG,"Alloc resources: %.2fms (%.2f ms skin load, %.2f ms preload)", 1000.f * (end - start) / freq, 1000.f * (slend - start) / freq, 1000.f * (plend - slend) / freq);
   else
   {
     CLog::Log(LOGDEBUG,"Window %s was already loaded", GetProperty("xmlfile").c_str());
-    CLog::Log(LOGDEBUG,"Alloc resources: %.2fm", 1000.f * (end.QuadPart - start.QuadPart) / freq.QuadPart);
+    CLog::Log(LOGDEBUG,"Alloc resources: %.2fm", 1000.f * (end - start) / freq);
   }
 #endif
   m_bAllocated = true;

xbmc/guilib/TextureManager.cpp
@@ -434,8 +434,8 @@ int CGUITextureManager::Load(const CStdString& strTextureName, bool checkBundleO
   LPDIRECT3DPALETTE8 pPal = 0;
 
 #ifdef _DEBUG
-  LARGE_INTEGER start;
-  QueryPerformanceCounter(&start);
+  int64_t start;
+  start = CurrentHostCounter();
 #endif
 
   D3DXIMAGE_INFO info;
@@ -553,11 +553,11 @@ int CGUITextureManager::Load(const CStdString& strTextureName, bool checkBundleO
     }
 
 #ifdef _DEBUG
-    LARGE_INTEGER end, freq;
-    QueryPerformanceCounter(&end);
-    QueryPerformanceFrequency(&freq);
+    int64_t end, freq;
+    end = CurrentHostCounter();
+    freq = CurrentHostFrequency();
     char temp[200];
-    sprintf(temp, "Load %s: %.1fms%s\n", strPath.c_str(), 1000.f * (end.QuadPart - start.QuadPart) / freq.QuadPart, (bundle >= 0) ? " (bundled)" : "");
+    sprintf(temp, "Load %s: %.1fms%s\n", strPath.c_str(), 1000.f * (end - start) / freq, (bundle >= 0) ? " (bundled)" : "");
     OutputDebugString(temp);
 #endif
 
@@ -636,11 +636,11 @@ int CGUITextureManager::Load(const CStdString& strTextureName, bool checkBundleO
   m_vecTextures.push_back(pMap);
 
 #ifdef _DEBUG
-  LARGE_INTEGER end, freq;
-  QueryPerformanceCounter(&end);
-  QueryPerformanceFrequency(&freq);
+  int64_t end, freq;
+  end = CurrentHostCounter();
+  freq = CurrentHostFrequency();
   char temp[200];
-  sprintf(temp, "Load %s: %.1fms%s\n", strPath.c_str(), 1000.f * (end.QuadPart - start.QuadPart) / freq.QuadPart, (bundle >= 0) ? " (bundled)" : "");
+  sprintf(temp, "Load %s: %.1fms%s\n", strPath.c_str(), 1000.f * (end - start) / freq, (bundle >= 0) ? " (bundled)" : "");
   OutputDebugString(temp);
 #endif
 

xbmc/utils/BitstreamStats.cpp
@@ -19,11 +19,9 @@
  */
 
 #include "BitstreamStats.h"
-#ifdef _LINUX
-#include "linux/XTimeUtils.h"
-#endif
+#include "utils/TimeUtils.h"
 
-LARGE_INTEGER BitstreamStats::m_tmFreq;
+int64_t BitstreamStats::m_tmFreq;
 
 BitstreamStats::BitstreamStats(unsigned int nEstimatedBitrate)
 {
@@ -33,10 +31,10 @@ BitstreamStats::BitstreamStats(unsigned int nEstimatedBitrate)
 
   m_nBitCount = 0;
   m_nEstimatedBitrate = nEstimatedBitrate;
-  m_tmStart.QuadPart = 0LL;
+  m_tmStart = 0LL;
 
-  if (m_tmFreq.QuadPart == 0LL)
-    QueryPerformanceFrequency(&m_tmFreq);
+  if (m_tmFreq == 0LL)
+    m_tmFreq = CurrentHostFrequency();
 }
 
 BitstreamStats::~BitstreamStats()
@@ -58,15 +56,15 @@ void BitstreamStats::AddSampleBits(unsigned int nBits)
 void BitstreamStats::Start()
 {
   m_nBitCount = 0;
-  QueryPerformanceCounter(&m_tmStart);
+  m_tmStart = CurrentHostCounter();
 }
 
 void BitstreamStats::CalculateBitrate()
 {
-  LARGE_INTEGER tmNow;
-  QueryPerformanceCounter(&tmNow);
+  int64_t tmNow;
+  tmNow = CurrentHostCounter();
 
-  double elapsed = (double)(tmNow.QuadPart - m_tmStart.QuadPart) / (double)m_tmFreq.QuadPart;
+  double elapsed = (double)(tmNow - m_tmStart) / (double)m_tmFreq;
   // only update once every 2 seconds
   if (elapsed >= 2)
   {

commit 1bdd1eefe7985dce6687464b0c20d44901f02884

fixed: video playback was broken after 43bf25fad718d20073f69db1f40a99f2ab3a4dee
- CurrentHostFrequency was used on places where CurrentHostCounter should be used.

xbmc/cores/dvdplayer/DVDClock.cpp
@@ -84,7 +84,7 @@ void CDVDClock::SetSpeed(int iSpeed)
   if(iSpeed == DVD_PLAYSPEED_PAUSE)
   {
     if(!m_pauseClock)
-      m_pauseClock = CurrentHostFrequency();
+      m_pauseClock = CurrentHostCounter();
     return;
   }
   
@@ -105,7 +105,7 @@ void CDVDClock::SetSpeed(int iSpeed)
 void CDVDClock::Discontinuity(double currentPts)
 {
   CExclusiveLock lock(m_critSection);
-  m_startClock = CurrentHostFrequency();
+  m_startClock = CurrentHostCounter();
   if(m_pauseClock)
     m_pauseClock = m_startClock;
   m_iDisc = currentPts;
@@ -116,7 +116,7 @@ void CDVDClock::Pause()
 {
   CExclusiveLock lock(m_critSection);
   if(!m_pauseClock)
-    m_pauseClock = CurrentHostFrequency();
+    m_pauseClock = CurrentHostCounter();
 }
 
 void CDVDClock::Resume()